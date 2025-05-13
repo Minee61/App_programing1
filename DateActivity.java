29cff8c44c649bb9e1327f431c0da1be7cc2dce3
Minee61 <lsm4080610@gmail.com>
2025-05-13 14:40:25 +0900

    Add files via upload

---
+++ DateActivity.java
@@ -0,0 +1,25 @@
+package com.example.login_intent2;
+
+import android.os.Bundle;
+import android.widget.LinearLayout;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class DateActivity  extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_intent);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main_date), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+    }
+}
